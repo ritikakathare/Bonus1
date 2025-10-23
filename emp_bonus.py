d612048fec07542dfe6af54f37d8bf1fa2ab063f
ritikakathare <ritikakathare15@gmail.com>
2025-10-23 17:08:16 +0530

    save to tax-deduction branch

--- emp_bonus.py
+++ emp_bonus.py
@@ -1,4 +1,8 @@
 # Salary Bonus Calculator Program
 
 salary = float(input("Enter employee's salary: "))
+bonus = salary * 0.10
+total_salary = salary + bonus
 print("Original Salary:", salary)
+print("Bonus Added:", total_salary)
+
